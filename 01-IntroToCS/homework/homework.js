9e8da005a79964158d8d7f5f782a5f81810c458a
Dac1977 <diegodac77@gmail.com>
2022-08-03 00:31:42 -0300

    segundo commit

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,13 +1,30 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  // tu codigo aca
-
+  // tu codigo aca 
+   let sum = 0;
+  for (let i = 0; i < num.length; i++) {
+    sum = sum + num[i] * Math.pow(2,num.length - 1 -i);
+    
+  }
+  
+  return sum;
+    
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
 
+let completo = []; 
+while (num !== 0) {
+  
+  let d = Math.floor(num / 2);
+  let rest = parseInt(num % 2);
+  completo.push(rest);
+  num = d;
+}
+let bin = completo.reverse().join("") ;
+return bin;
 }
 
 
